62f3885c9ad6016796e04ae5f13d2df15ac7ffe9
DucAn <annguyen2422004@gmail.com>
2024-01-26 10:07:26 +0700

    nhanh DucAn

--- obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea05aff60ba4e2916e89b00b3ecad36eed4a619c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc311d79307a0382386025bfbb47ea6883948e1b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
